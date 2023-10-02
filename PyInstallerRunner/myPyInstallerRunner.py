200c913c8752efd0aad2973314adab45d9c93a21
burstknight <burstknight@gamil.com>
2023-10-02 23:13:05 +0800

    feat: Add methods to load yaml file
    - Target:
      The class `myPyInstallerRunner` need methods to load configuration
      from a yaml file.
    
    - Changed:
      - Add the method `loadSetting()` into the class `myPyInstallerRunner`
        to load `*.yaml`.
      - Add the method `__setSettingTree()` into the class
        `myPyInstallerRunner`.
      - Add the method `__setSettingNode()` into the class
        `myPyInstallerRunner`.

--- PyInstallerRunner/myPyInstallerRunner.py
+++ PyInstallerRunner/myPyInstallerRunner.py
@@ -1,4 +1,6 @@
 from typing import Dict, Union
+from yaml import safe_load
+from os.path import isfile
 
 class myPyInstallerRunner(object):
     """
@@ -13,7 +15,79 @@ class myPyInstallerRunner(object):
     # End of constructor
 
     @property
-    def m_dctSettings(self) -> Dict[str, Union[str, bool]]:
+    def m_dctSettings(self) -> Dict[str, Union[str, bool, Dict]]:
         return self.__m_dctSettings
     # End of myPyInstallerRunner::m_dctSettings
+
+    def loadSetting(self, strPath:str):
+        """
+        Description:
+        ===================================================
+        Load the setting from the given file path.
+
+        Args:
+        ===================================================
+        - strPath: Give the setting file path.
+        """
+        if False == isfile(strPath):
+            raise FileNotFoundError("Not found file: %s" %(strPath))
+        # End of if-condition
+
+        dctYamlSetting = safe_load(strPath)
+
+        self.__setSettingTree(dctYamlSetting, self.m_dctSettings)
+    # End of myPyInstallerRunner::loadConfig
+
+    def __setSettingTree(self, dctYamlTree: Dict, dctSettingTree: Dict):
+        """
+        Description:
+        ===================================================
+        Use tree travel to set the setting from the yaml file.
+
+        Args:
+        ===================================================
+        - dctYamlTree: Give the context for the yaml file.
+        - dctSettingTree: Give the setting.
+        """
+        if False == isinstance(dctYamlTree, dict):
+            raise TypeError("The type of the parameter `dctYamlTree` must be `dict` in the method `__setSettingNode()` of the class `myPyInstallerRunner`!")
+        # End of if-condition
+
+        if False == isinstance(dctSettingTree, dict):
+            raise TypeError("The type of the parameter `dctSettingTree` must be `dict` in the method `__setSettingNode()` of the class `myPyInstallerRunner`!")
+        # End of if-condition
+
+        for strKey in dctYamlTree.keys():
+            self.__setSettingNode(strKey= strKey, dctYamlNode=dctYamlTree[strKey], dctSettingNode=dctSettingTree[strKey])
+        # End of for-loop
+    # End of myPyInstallerRunner::setSettingNode
+
+    def __setSettingNode(self, strKey: str , dctYamlNode: Dict, dctSettingNode: Dict):
+        """
+        Description:
+        ===================================================
+        Use recursive to set the settings.
+
+        Args:
+        ===================================================
+        - strKey: Give the key word to set the value for the setting.
+        - dctYamlNode: Give the node from the yaml file.
+        - dctSettingNode: Give the setting node to set.
+        """
+        isLeaf = False
+        try:
+            dctYamlNode.items()
+        except:
+            isLeaf = True
+        # End of try-catch
+
+        if True == isLeaf:
+            dctSettingNode[strKey] = dctYamlNode[strKey]
+            return
+        else:
+            for strSonKey in dctYamlNode.keys():
+                self.__setSettingNode(strKey, dctYamlNode[strSonKey], dctSettingNode[strSonKey])
+            # End of for-loop
+        # End of if-condition
+    # End of myPyInstallerRunner::setSettingNode
 # End of class myPyInstallerRunner
