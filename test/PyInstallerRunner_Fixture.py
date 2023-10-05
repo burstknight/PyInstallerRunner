cfd1c9db7ddf1429989a58308df94a375533acb8
burstknight <burstknight@gamil.com>
2023-10-05 16:17:48 +0800

    test: Add unit test to test loading config file
    - Target:
      This repo need an unit test to test loading config file for the class
      `myPyInstallerRunner`.
    
    - Changed:
      Add the unit test `test_PyInstallerRunner_loadConfig`.

---
+++ test/PyInstallerRunner_Fixture.py
@@ -0,0 +1,29 @@
+from typing import Dict
+import pytest
+
+@pytest.fixture()
+def test_dctDefaultSettings() -> Dict:
+    dctSettings = {}
+    dctSettings["BuildPath"] = {"DistPath": "./bin/release", "SpecPath": "./bin", "WorkPath": "./bin/build", "IconPath": ""}
+    dctSettings["Args"] = {"IsFile": True, "NeedShowConsole": True}
+
+    return dctSettings
+# End of test_dctDefaultSettings
+
+@pytest.fixture()
+def test_dctCompleteSettings() -> Dict:
+    dctSettings = {}
+    dctSettings["BuildPath"] = {"DistPath": "./build/release", "SpecPath": "./build", "WorkPath": "./build/tmp", "IconPath": "./resources/image.icon"}
+    dctSettings["Args"] = {"IsFile": False, "NeedShowConsole": True}
+
+    return dctSettings
+# End of test_dctCompleteSettings
+
+@pytest.fixture()
+def test_dctPartSettings():
+    dctSettings = {}
+    dctSettings["BuildPath"] = {"DistPath": "./bin/release", "SpecPath": "./bin", "WorkPath": "./build/tmp", "IconPath": "./resources/image.icon"}
+    dctSettings["Args"] = {"IsFile": False, "NeedShowConsole": True}
+
+    return dctSettings
+# End of test_dctPartSettings
---
+++ test/test_PyInstallerRunner_loadConfig.py
@@ -0,0 +1,51 @@
+import pytest
+from PyInstallerRunner.myPyInstallerRunner import myPyInstallerRunner
+from test.PyInstallerRunner_Fixture import test_dctDefaultSettings, test_dctCompleteSettings, test_dctPartSettings
+
+def test_PyInstallerRunner_SucceedDefault(test_dctDefaultSettings):
+    oRunner = myPyInstallerRunner()
+    assert oRunner.m_dctSettings == test_dctDefaultSettings
+# End of test_PyInstallerRunner_succeed_default
+
+def test_PyInstallerRunner_SucceedComplete(test_dctCompleteSettings):
+    oRunner = myPyInstallerRunner()
+    oRunner.loadSetting("./test/configs/complete_config.yaml")
+
+    assert oRunner.m_dctSettings == test_dctCompleteSettings
+# End of test_PyInstallerRunner_succeed_complete
+
+def test_PyInstallerRunner_SucceedPart(test_dctPartSettings):
+    oRunner = myPyInstallerRunner()
+    oRunner.loadSetting("./test/configs/part_config.yaml")
+
+    assert oRunner.m_dctSettings == test_dctPartSettings
+# End of test_PyInstallerRunner_succeed_part
+
+def test_PyInstallerRunner_KeyNoExists():
+    with pytest.raises(KeyError):
+        oRunner = myPyInstallerRunner()
+        oRunner.loadSetting("./test/configs/no_key_config.yaml")
+    # End of with-block
+# End of test_PyInstallerRunner_KeyNoExists
+
+def test_PyInstallerRunner_setSettingTree_dctYamlTree_error():
+    with pytest.raises(TypeError):
+        oRunner = myPyInstallerRunner()
+        oRunner._myPyInstallerRunner__setSettingTree(1, {})
+    # End of with-block
+# End of test_PyInstallerRunner_setSettingTree_dctYamlTree_error
+
+def test_PyInstallerRunner_setSettingTree_dctSettingTree_error():
+    with pytest.raises(TypeError):
+        oRunner = myPyInstallerRunner()
+        oRunner._myPyInstallerRunner__setSettingTree({}, 1)
+    # End of with-block
+# End of test_PyInstallerRunner_setSettingTree_dctYamlTree_error
+
+def test_PyInstallerRunner_NotFoundFile():
+    with pytest.raises(FileNotFoundError):
+        oRunner = myPyInstallerRunner()
+        oRunner.loadSetting("test")
+    # End of with-block
+# End of test_PyInstallerRunner_NotFoundFile
+
