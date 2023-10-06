8dae50b336ec6843b78a552dbf7859657a842eab
burstknight <burstknight@gamil.com>
2023-10-06 10:55:09 +0800

    test: Add unit test for `myPyInstallerRunner.__parseSettings()`
    - Target:
      This repo need an unit test to test the method `__parseSettings()` of
      the class `myPyInstallerRunner`.
    
    - Changed:
      Add the unit test `test_PyInstaller_ParseArgs`.

--- test/PyInstallerRunner_Fixture.py
+++ test/PyInstallerRunner_Fixture.py
@@ -23,7 +23,23 @@ def test_dctCompleteSettings() -> Dict:
 def test_dctPartSettings():
     dctSettings = {}
     dctSettings["BuildPath"] = {"DistPath": "./bin/release", "SpecPath": "./bin", "WorkPath": "./build/tmp", "IconPath": "./resources/image.icon"}
-    dctSettings["Args"] = {"IsFile": False, "NeedShowConsole": True}
+    dctSettings["Args"] = {"IsFile": True, "NeedShowConsole": False}
 
     return dctSettings
 # End of test_dctPartSettings
+
+@pytest.fixture()
+def test_strDefaultArgs() -> str:
+    return "-F --distpath \"./bin/release\" --specpath \"./bin\" --workpath \"./bin/build\" "
+# End of test_strDefaultArgs
+
+@pytest.fixture()
+def test_strCompleteArgs() -> str:
+    return "-D --distpath \"./build/release\" --specpath \"./build\" --workpath \"./build/tmp\" -i \"./resources/image.icon\" "
+# End of test_strCompleteArgs
+
+@pytest.fixture()
+def test_strPartArgs() -> str:
+    return "-F -w --distpath \"./bin/release\" --specpath \"./bin\" --workpath \"./build/tmp\" -i \"./resources/image.icon\" "
+# End of test_strPartArgs
+
